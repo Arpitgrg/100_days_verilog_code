a24d8a6f52a10e787b316c6b27ca0025e2b36c80
Arpitgrg <146650450+Arpitgrg@users.noreply.github.com>
2023-11-27 18:37:27 +0530

    Update priority_encoder_casex.v

--- Day11/priority_encoder_casex.v
+++ Day11/priority_encoder_casex.v
@@ -32,11 +32,12 @@ module priority_encoder_casex(
         end
     else
         begin
-        case(d_in)
+        casex(d_in)
         4'b0001:y_out=2'b00;
         4'b001x:y_out=2'b01;
         4'b01xx:y_out=2'b10;
         4'b1xxx:y_out=2'b11;
+        default: y_out=2'bxx;    
         endcase
         end
 
